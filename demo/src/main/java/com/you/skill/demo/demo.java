1990d12e29077080327bf0f5000b59998067f07a
youxiaojia <718586216@qq.com>
2018-10-26 22:09:36 +0800

    init.

---
+++ demo/src/main/java/com/you/skill/demo/demo.java
@@ -0,0 +1,35 @@
+/**
+ * <pre>
+ * 作   者：youxiaojia
+ * 创建日期：2018/10/26
+ * </pre>
+ */
+/*                                                                        */
+/* Copyright (c) 2017 KYE Company                                       */
+/* 跨越速运集团有限公司版权所有                                           */
+/*                                                                        */
+/* PROPRIETARY RIGHTS of KYE Company are involved in the                */
+/* subject matter of this material. All manufacturing, reproduction, use, */
+/* and sales rights pertaining to this subject matter are governed by the */
+/* license agreement. The recipient of this software implicitly accepts   */
+/* the terms of the license.                                              */
+/* 本软件文档资料是跨越速运集团有限公司的资产，任何人士阅读和                   */
+/* 使用本资料必须获得相应的书面授权，承担保密责任和接受相应的法律约束。                 */
+/*                                                                        */
+/**************************************************************************/
+
+/**
+ * <pre>
+ * 作   者：youxiaojia
+ * 创建日期：2018/10/26
+ * </pre>
+ */
+
+
+package com.you.skill.demo;
+
+public class demo {
+    public static void main(String[] args) {
+        System.out.println("hello world.");
+    }
+}
